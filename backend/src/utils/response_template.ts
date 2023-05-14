9fdbf258a0d9958dfae340533f4a27fed7be25fb
Si Zhang <szhang@SideMacBook-Pro.local>
2023-05-13 20:51:55 -0700

    add ssl files and some fe folders

---
+++ backend/src/utils/response_template.ts
@@ -0,0 +1,7 @@
+// this file we can check the fromat of the response from the open api.
+// and determine what we want to return to the frontend.
+// for example if response contians python code, we need a code block to display it.
+
+
+
+// should we make this check in FE or here?
