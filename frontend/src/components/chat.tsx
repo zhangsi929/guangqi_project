04894b6e5efefce895c6b2a34b9cb0495f3e5a5e
Si Zhang <szhang@SideMacBook-Pro.local>
2023-05-14 04:53:03 -0700

    good one

--- frontend/src/components/chat.tsx
+++ frontend/src/components/chat.tsx
@@ -1,36 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { FaPaperPlane } from 'react-icons/fa';
 
-function Chat() {
-    const [messages, setMessages] = useState('');
+const Chat: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [messages, setMessages] = useState('');
+  const esRef = useRef<EventSource | null>(null);
 
-    useEffect(() => {
-        // const es = new EventSource(`http://localhost:3001/api/streamChat?prompt=${encodeURIComponent(prompt)}`);
-        const es = new EventSource(`http://localhost:3001/api/streamChat?prompt=hahahah`);
-        es.onopen = function () {
-            const message = 'Custom prompt message'; // Set your custom prompt message here
-        };
+  useEffect(() => {
+    return () => {
+      // Close the EventSource when the component is unmounted
+      if (esRef.current) esRef.current.close();
+    };
+  }, []);
 
-        es.onmessage = function (event) {
-            const message = event.data;
-            //   debugger
-            if (message == 'AIChatTermination20230514flexva') {
-                es.close();
-            } else {
-                setMessages(prevMessages => `${prevMessages} ${message}`);
-            }
-        };
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
-        return () => {
-            es.close();
-        };
-    }, []);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (esRef.current) esRef.current.close();
+    esRef.current = new EventSource(`http://localhost:3001/api/streamChat?prompt=${encodeURIComponent(inputValue)}`);
+    
+    esRef.current.onmessage = function (event) {
+      const message = event.data;
+      if (message === 'AIChatTermination20230514flexva') {
+        if (esRef.current) esRef.current.close();
+      } else {
+        setMessages((prevMessages) => prevMessages + '\n' + message);
+      }
+    };
 
-    return (
-        <div>
-            <h1>Response:</h1>
-            {messages}
+    esRef.current.onerror = function (event) {
+      console.error("EventSource failed:", event);
+      if (esRef.current) esRef.current.close();
+    };
+    // Clear the input field after submission
+    setInputValue('');
+  };
+
+  return (
+    <div className="container">
+      <div className="logo">ChatGPT API Demo</div>
+      <form onSubmit={handleSubmit} className="input-group">
+        <input
+          type="text"
+          placeholder="Type your message here..."
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button type="submit">
+          <FaPaperPlane />
+        </button>
+      </form>
+      <div className="chat-box">
+        <div className="message-container">
+          <p className="text-lg">{messages}</p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Chat;
