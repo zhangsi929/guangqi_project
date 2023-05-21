6467401ed5d7b6bd78cfbbcac124b8b7b52afc6a
Si Zhang <szhang@SideMacBook-Pro.local>
2023-05-20 17:51:18 -0700

    fe

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Ethan Zhang
  * @Date: 2023-05-19 23:18:49
- * @LastEditTime: 2023-05-20 16:53:44
+ * @LastEditTime: 2023-05-20 17:50:03
  * @FilePath: /guangqi/client/src/pages/_app.tsx
  * @Description:
  *
@@ -15,42 +15,63 @@ import {
   QueryClientProvider,
   QueryCache,
 } from "@tanstack/react-query";
-import { ApiErrorBoundaryProvider } from "../hooks/ApiErrorBoundaryContext";
+import {
+  ApiErrorBoundaryProvider,
+  useApiErrorBoundary,
+} from "../hooks/ApiErrorBoundaryContext";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "../hooks/ThemeContext";
 import { ScreenshotProvider } from "../utils/screenshotContext";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import React from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+type MyError = {
+  response?: {
+    status?: number;
+  };
+};
+
+function App({ Component, pageProps }: AppProps) {
+  const { setError } = useApiErrorBoundary();
   const queryClient = new QueryClient({
     queryCache: new QueryCache({
-      onError: (error) => {
-        if (error?.response?.status === 401) {
-          // This error handling will be different in Next.js. You'll probably want to use Next.js' router here.
-          // we want all pages to redirect to login if the user is not authenticated????
-          router.push("/login");
+      onError: (error: unknown) => {
+        if (
+          typeof error === "object" &&
+          error !== null &&
+          "response" in error
+        ) {
+          const err = error as MyError;
+          if (err.response?.status === 401) {
+            setError(error);
+          }
         }
       },
     }),
   });
 
   return (
+    <ScreenshotProvider>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <ThemeProvider initialTheme="light">
+            <Component {...pageProps} />
+            {process.env.NODE_ENV === "development" && (
+              <ReactQueryDevtools initialIsOpen={false} />
+            )}
+          </ThemeProvider>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </ScreenshotProvider>
+  );
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
     <ApiErrorBoundaryProvider>
-      <ScreenshotProvider>
-        <QueryClientProvider client={queryClient}>
-          <RecoilRoot>
-            <ThemeProvider>
-              <Component {...pageProps} />
-              {process.env.NODE_ENV === "development" && (
-                <ReactQueryDevtools initialIsOpen={false} />
-              )}
-            </ThemeProvider>
-          </RecoilRoot>
-        </QueryClientProvider>
-      </ScreenshotProvider>
+      <App Component={Component} pageProps={pageProps} />
     </ApiErrorBoundaryProvider>
   );
 }
