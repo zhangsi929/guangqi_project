210ab4a4dab87f32e3dff688df92240d498d2016
Si Zhang <szhang@SideMacBook-Pro.local>
2023-05-25 00:06:18 -0700

    .

--- newbackend/strategies/jwtStrategy.js
+++ newbackend/strategies/jwtStrategy.js
@@ -1,9 +1,11 @@
 /*
  * @Author: Ethan Zhang
  * @Date: 2023-05-23 21:08:44
- * @LastEditTime: 2023-05-24 18:26:39
+ * @LastEditTime: 2023-05-25 00:05:38
  * @FilePath: /guangqi/newbackend/strategies/jwtStrategy.js
  * @Description: 
+ * 
+ * Used as a GATE for all protected routes
  * 
  * Copyright (c) 2023 Ethan Zhang, All Rights Reserved. 
  */
@@ -15,13 +17,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 const secretOrKey = isProduction ? process.env.JWT_SECRET_PROD : process.env.JWT_SECRET_DEV;
 
 // JWT strategy
-
+const jwtLogin = new JwtStrategy(
   {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey
   },
   async (payload, done) => {
-    console.log('JwtStrategy => payload: ', payload);
     try {
       const user = await User.findById(payload.id);
       if (user) {
